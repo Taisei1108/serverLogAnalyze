271fa8a53d34e7eb154b1157d1394ca526ce289a
Taisei1108 <taikitigw7@gmail.com>
2021-02-12 02:35:13 +0900

    No1の作成

--- load.py
+++ load.py
@@ -3,17 +3,20 @@
 def load_log(path):
     with open(path) as f:
         log_all = [s.strip() for s in f.readlines()]
-        print(type(f))
 
     #エラー処理追加したい2021年2月11日
+    num = []
     date = []
     network = []
     state = []
+    count = 0
     for i in log_all:
         s = i.split(',')
+        num.append(count)
         date.append(s[0])
         network.append(s[1])
         state.append(s[2])
+        count+=1
 
-    log_dict = {'date':date,'network':network,'state':state}
+    log_dict = {'num':num,'date':date,'network':network,'state':state}
     return log_dict
